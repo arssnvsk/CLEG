870be4bc645af6fd537ef71a05c6c9c1797311f5
Artsiom <artsiom@dortal.co.il>
2022-11-13 12:48:05 +0200

    change selling oder

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const TOKEN = localStorage.getItem('user.token')
 const BASE_URL = 'https://core.chainoflegends.com/api/v1'
 const ORDER_ENDPOINT = '/land/2/orders'
 const PLAYER_ENDPOINT = '/player'
+const PLAYER_LAND_ENDPOINT = '/playerland'
 const BUY_ORDER_ENDPOINT = (id) => '/order/' + id + '/buy'
 const SELL_ORDER_ENDPOINT = (id) => '/playerland/' + id + '/place-order'
 const MAX_COMMON_PRICE = 34
-const SELL_PRICE = (36.4 + Math.random() * 0.1).toFixed(2)
 
 const http = async ({ endpoint, config = {} }) => {
   const defaultConfig = {
@@ -48,33 +48,46 @@ const buyOrders = async (orders) => {
   for (const { ID } of orders) {
     try {
       await http({ endpoint: BUY_ORDER_ENDPOINT(ID) })
-      await sellOrder(ID)
     } catch (e) {
       console.log(e)
     }
   }
 }
 
-const sellOrder = async (ID) => {
+const getNotSellingOrders = async () => {
+  const orders = await http({ endpoint: PLAYER_LAND_ENDPOINT })
+  const notSellingorders = orders.filter(({ clegPrice }) => clegPrice === 0)
+  console.log('not sell orders', notSellingorders)
+  return notSellingorders
+}
+
+const sellOrders = async () => {
+  const SELL_PRICE = (36.4 + Math.random() * 0.1).toFixed(2)
   const sellOrderConfig = {
     method: 'POST',
     body: JSON.stringify({ clegPrice: SELL_PRICE })
   }
-  try {
-    const res = await http({
-      endpoint: SELL_ORDER_ENDPOINT(ID),
-      config: sellOrderConfig
-    })
-    console.log('set to sell', res)
-  } catch (e) {
-    console.log(e)
+
+  const orders = getNotSellingOrders()
+  for (const { id } of orders) {
+    try {
+      const res = await http({
+        endpoint: SELL_ORDER_ENDPOINT(id),
+        config: sellOrderConfig
+      })
+      console.log('set to sell', res)
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
 
 const app = async () => {
   const { totalCleg } = await getPlayerInfo()
   const buylableOrders = await getBuylableOrders(totalCleg)
-  buyOrders(buylableOrders)
+  if (!buylableOrders.length) return
+  await buyOrders(buylableOrders)
+  await sellOrders()
 }
 setInterval(() => {
   app()
